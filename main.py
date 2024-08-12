55b9b74ab58dc8f8be47edbaf3e09ebbdb07a1f1
Alexander K. <alexander.kolyadko@gmail.com>
2024-08-12 23:27:52 +0300

    fix: resolve flake8 errors

--- main.py
+++ main.py
@@ -2,7 +2,6 @@
 import psycopg2
 from psycopg2 import sql
 import requests
-import time
 import datetime
 
 conn = psycopg2.connect(
@@ -12,15 +11,6 @@ conn = psycopg2.connect(
     host='ella.db.elephantsql.com'
 )
 cur = conn.cursor()
-
-
-#cur.execute('''
-    #CREATE TABLE IF NOT EXISTS hash_table (
-        #id SERIAL PRIMARY KEY,
-        #hash VARCHAR(255),
-        #added_at TIMESTAMP DEFAULT current_timestamp
-    #)
-#''')
 
 api_key = '7a65bfa6-750b-4490-be79-b47a31c235a9'
 endpoint = 'https://apilist.tronscanapi.com/api/transaction-info'
@@ -60,33 +50,61 @@ if response.status_code == 200:
             to_address = trc20_info[0].get('to_address')
             timestamp = data.get('timestamp')
 
-            #print('Сумма в блокчейне:', amount_str)
-            #print('Получатель средств:', to_address)
+            # print('Сумма в блокчейне:', amount_str)
+            # print('Получатель средств:', to_address)
 
             if to_address != 'TM4GjqKvoneHmvMYoLVjRXMdUQo5nCe55x':
-                print('⚠️⚠️⚠️ Внимание: Кошелёк получателя отличается от TM4GjqKvoneHmvMYoLVjRXMdUQo5nCe55x ⚠️⚠️⚠️')
+                print
+                (
+                    '''
+                      ⚠️⚠️⚠️ Внимание:
+                      Кошелёк получателя отличается от
+                      TM4GjqKvoneHmvMYoLVjRXMdUQo5nCe55x ⚠️⚠️⚠️
+                    '''
+                )
             else:
-                print('✅ Перевод выполнен на актуальный кошелёк TM4GjqKvoneHmvMYoLVjRXMdUQo5nCe55x ✅')
+                print
+                (
+                    '''
+                    ✅ Перевод выполнен на актуальный кошелёк
+                    TM4GjqKvoneHmvMYoLVjRXMdUQo5nCe55x ✅
+                    '''
+                )
 
                 if 'confirmed' in data and 'timestamp' in data:
                     confirmed_value = data['confirmed']
 
-                    if confirmed_value == True:
+                    if confirmed_value is True:
                         print('✅ Перевод выполнен успешно ✅')
 
-                        transaction_time = datetime.datetime.fromtimestamp(int(timestamp) / 1000.0)
+                        transaction_time = datetime.datetime.fromtimestamp(
+                            int(timestamp) / 1000.0
+                        )
                         current_time = datetime.datetime.now()
-
-                        if current_time - transaction_time > datetime.timedelta(hours=1):
-                            print('⚠️⚠️⚠️ Этот перевод старше одного часа. Возможно дублирование HASH от партнёра ⚠️⚠️⚠️')
-                        if current_time - transaction_time < datetime.timedelta(hours=1):
+                        transatction_delta = current_time - transaction_time
+                        if (
+                            transatction_delta > datetime.timedelta(hours=1)
+                        ):
+                            print(
+                                '''
+                                ⚠️⚠️⚠️ Этот перевод старше одного часа.
+                                Возможно дублирование HASH от партнёра ⚠️⚠️⚠️
+                                '''
+                            )
+                        if transatction_delta < datetime.timedelta(hours=1):
                             print('✅ Перевод выполнен недавно ✅')
 
-                        num_amount = float(amount_str[:-6] + '.' + amount_str[-6:])
+                        num_amount = float(
+                            amount_str[:-6] + '.' + amount_str[-6:]
+                        )
 
-                        exchange_rate = float(input('Введите курс для расчета суммы: '))
+                        exchange_rate = float(
+                            input('Введите курс для расчета суммы: ')
+                        )
 
-                        tariff_percent = float(input('Введите процент по тарифу: '))
+                        tariff_percent = float(
+                            input('Введите процент по тарифу: ')
+                        )
 
                         total = num_amount * exchange_rate
                         total = round(total, 2)
@@ -95,18 +113,27 @@ if response.status_code == 200:
                         total2 = round(total2, 2)
 
                         if total2.is_integer():
-                          total2 = int(total2)
+                            total2 = int(total2)
 
-                        print(f"{num_amount} * {exchange_rate} = {total} + {tariff_percent}% = {total2 if isinstance(total2, int) else total2:.2f}")
+                        print(
+                            f'''{num_amount} * {exchange_rate}
+                            = {total} + {tariff_percent}% =
+                            {total2 if isinstance(total2, int)
+                            else total2:.2f}'''
+                        )
 
                         cur.execute(
-                            sql.SQL("INSERT INTO hash_table (hash) VALUES (%s)")
-                                .format(sql.Identifier('hash')),
+                            sql.SQL(
+                                "INSERT INTO hash_table (hash) VALUES (%s)"
+                            ).format(sql.Identifier('hash')),
                             [transaction_hash]
                         )
                         conn.commit()
                 else:
-                    print('❌ Информация о подтверждении перевода отсутствует в ответе API ❌')
+                    print(
+                        '''❌ Информация о подтверждении перевода отсутствует
+                        в ответе API ❌'''
+                    )
         else:
             print('❌ Информация о переводе TRC20 отсутствует в ответе API ❌')
     else:
